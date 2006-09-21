3b0b945b9d07cac8feaf08008c6a36d40afea065
Joshua Davis <pgmjsd@gmail.com>
2006-09-21 21:29:46 +0000

    Next iteration.
    git-svn-id: https://svn.berlios.de/svnroot/repos/jgrid/trunk@26 66cbbda0-d104-0410-90a5-e1a569055c76

---
+++ jegrid/src/org/jegrid/impl/GridImpl.java
@@ -0,0 +1,14 @@
+package org.jegrid.impl;
+
+import org.jegrid.Grid;
+
+/**
+ * TODO: Add class level javadoc.
+ * <br>User: jdavis
+ * Date: Sep 21, 2006
+ * Time: 5:34:18 PM
+ */
+public class GridImpl implements Grid
+{
+    
+}
