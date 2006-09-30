1efcae069123946e1e871c33ba3e9319540b2e18
Joshua Davis <pgmjsd@gmail.com>
2006-09-30 15:01:29 +0000

    upgrade to jgroups 2.2.9
    git-svn-id: https://svn.berlios.de/svnroot/repos/jgrid/trunk@28 66cbbda0-d104-0410-90a5-e1a569055c76

--- jegrid/src/org/jegrid/jgroups/JGroupsBus.java
+++ jegrid/src/org/jegrid/jgroups/JGroupsBus.java
@@ -17,19 +17,7 @@ import org.jgroups.JChannel;
 public class JGroupsBus implements Bus
 {
     private static Logger log = Logger.getLogger(JGroupsBus.class);
-    private static final String DEFAULT_PROPS = "UDP1_4(mcast_addr=228.8.8.8;mcast_port=27000;ip_ttl=1;" +
-            "mcast_send_buf_size=64000;mcast_recv_buf_size=64000):" +
-            "PING(timeout=2000;num_initial_members=3):" +
-            "MERGE2(min_interval=5000;max_interval=10000):" +
-            "FD_SOCK:" +
-            "VERIFY_SUSPECT(timeout=1500):" +
-            "pbcast.NAKACK(max_xmit_size=8096;gc_lag=50;retransmit_timeout=300,600,1200,2400,4800):" +
-            "UNICAST(timeout=600,1200,2400,4800):" +
-            "pbcast.STABLE(desired_avg_gossip=20000):" +
-            "FRAG(frag_size=8096;down_thread=false;up_thread=false):" +
-            "pbcast.GMS(join_timeout=5000;join_retry_timeout=2000;" +
-            "shun=false;print_local_addr=true)";
-
+    private static final String DEFAULT_PROPS = "default.xml";
     private boolean running = false;
     private Channel channel;
     private GridConfiguration config;
